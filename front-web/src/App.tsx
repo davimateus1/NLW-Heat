5a83fba5f8323e79a15d8a4e9bf5cb7b30b9a044
Davi Mateus Gomes de Almeida <66326378+davimateus1@users.noreply.github.com>
2021-10-19 22:55:36 -0300

    Finalização - todas as integrações completas

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,12 +1,18 @@
+import { useContext } from "react";
 import styles from "./App.module.scss";
 import { LoginBox } from "./components/LoginBox";
 import { MessageList } from "./components/MessageList";
+import { SendMessageForm } from "./components/SendMessageForm";
+import { AuthContext } from "./contexts/auth";
 
 export function App() {
+
+  const { user } = useContext(AuthContext);
+
   return (
-    <main className={styles.contentWrapper}>
+    <main className={`${styles.contentWrapper} ${!!user ? styles.contentSigned : ''}`}>
       <MessageList />
-      <LoginBox />
+      { !!user ? <SendMessageForm/> : <LoginBox /> }
     </main>
   );
 }
--- front-web/src/components/MessageList/index.tsx
+++ front-web/src/components/MessageList/index.tsx
@@ -1,48 +1,68 @@
-import styles from './styles.module.scss';
-import logoImg from '../../assets/logo.svg'
+import { api } from "../../services/api";
+import io from "socket.io-client";
+import styles from "./styles.module.scss";
+import logoImg from "../../assets/logo.svg";
+import { useEffect, useState } from "react";
 
-export function MessageList(){
-    return (
-       <div className={styles.messageListWrapper}>
-           <img src={logoImg} alt="DoWhile2021" />
+type Message = {
+  id: string;
+  text: string;
+  user: {
+    name: string;
+    avatar_url: string;
+  };
+};
 
-           <ul className={styles.messageList}>
-                <li className= {styles.message}>
-                    <p className={styles.messageContent}>
-                    Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥  
-                    </p>
-                    <div className={styles.messageUser}>
-                        <div className={styles.userImage}>
-                            <img src="https://github.com/davimateus1.png" alt="" />
-                        </div>
-                        <span>Davi Mateus</span>
-                    </div>
-                </li>
+const messagesQueue: Message[] = [];
 
-                <li className= {styles.message}>
-                    <p className={styles.messageContent}>
-                    Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥  
-                    </p>
-                    <div className={styles.messageUser}>
-                        <div className={styles.userImage}>
-                            <img src="https://github.com/davimateus1.png" alt="" />
-                        </div>
-                        <span>Davi Mateus</span>
-                    </div>
-                </li>
+const socket = io("http://localhost:4000");
 
-                <li className= {styles.message}>
-                    <p className={styles.messageContent}>
-                    Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥  
-                    </p>
-                    <div className={styles.messageUser}>
-                        <div className={styles.userImage}>
-                            <img src="https://github.com/davimateus1.png" alt="" />
-                        </div>
-                        <span>Davi Mateus</span>
-                    </div>
-                </li>
-           </ul>
-       </div>
-    )
+socket.on("new_message", (newMessage: Message) => {
+  messagesQueue.push(newMessage);
+});
+
+export function MessageList() {
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  useEffect(() => {
+    setInterval(() => {
+      if(messagesQueue.length > 0){
+        setMessages(prevState => [
+          messagesQueue[0],
+          prevState[0],
+          prevState[1],
+        ].filter(Boolean))
+
+        messagesQueue.shift();
+      }
+    }, 3500)
+  }, [])
+
+  useEffect(() => {
+    api.get<Message[]>("messages/last3").then((response) => {
+      setMessages(response.data);
+    });
+  }, []);
+
+  return (
+    <div className={styles.messageListWrapper}>
+      <img src={logoImg} alt="DoWhile2021" />
+
+      <ul className={styles.messageList}>
+        {messages.map((message) => {
+          return (
+            <li key={message.id} className={styles.message}>
+              <p className={styles.messageContent}>{message.text}</p>
+              <div className={styles.messageUser}>
+                <div className={styles.userImage}>
+                  <img src={message.user.avatar_url} alt="" />
+                </div>
+                <span>{message.user.name}</span>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
